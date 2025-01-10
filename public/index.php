02d3e95861c489c74afb5752fdea97a2a8abef9a
Lenovo <lenovo@lenovo.com>
2025-01-10 09:25:55 +0100

    Idioma segun preferencia del usuario

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$availableLanguages = ['es', 'ca', 'en'];
+$defaultLanguage = 'es';
+
+$userLanguages = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+
+$languageList = [];
+foreach ($userLanguages as $language) {
+    $parts = explode(';q=', $language);
+    $languageCode = substr($parts[0], 0, 2);
+
+    if (in_array($languageCode, $availableLanguages)) {
+        $quality = isset($parts[1]) ? (float)$parts[1] : 1;
+        $languageList[] = ['language' => $languageCode, 'q' => $quality];
+    }
+}
+
+usort($languageList, function ($a, $b) {
+    return $b['q'] <=> $a['q'];
+});
+
+$userLanguage = !empty($languageList[0]) ? $languageList[0]['language'] : $defaultLanguage;
+
+header("Location: /$userLanguage", true, 302);
+exit;
